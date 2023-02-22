1fec6198d6579a4450334dbe9ef9a8a264cc3ade
Jacty <saej3101@usherbrooke.ca>
2023-02-22 14:55:27 -0500

    Added prob 2

--- code/labo.py
+++ code/labo.py
@@ -21,6 +21,28 @@ def prob1():
 
     w, amp = signal.freqz(num, denum)
 
+    rep_freq(w, amp)
+
+    impulsion = np.zeros(500)
+    impulsion[int(len(impulsion)/2)] = 1
+    plt.stem(impulsion)
+    plt.show()
+
+    h = filter_h(num, denum, impulsion, True)
+    h_inv = filter_h(denum, num, h)
+    plt.stem(h_inv)
+    plt.show()
+
+def filter_h(num, denum, sg, plot=False):
+    h = signal.lfilter(num, denum, sg)
+    
+    if plot:
+        plt.plot(h)
+        plt.show()
+
+    return h
+
+def rep_freq(w, amp):
     fig, ax1 = plt.subplots()
     ax1.set_title('Amplitude et phase de la réponse en fréquence')
     ax1.plot(w, 20 * np.log10(abs(amp)), 'r')
@@ -33,18 +55,40 @@ def prob1():
     ax2.set_ylabel('Angle (rad)', color='b')
     plt.show()
 
-    impulsion = np.zeros(500)
-    impulsion[int(len(impulsion)/2)] = 1
-    plt.stem(impulsion)
+
+def prob2():
+    pi_16 = np.pi/16
+    z = [ np.exp(1j*pi_16), np.exp(1j*(-pi_16)) ] 
+    p = [ 0.95*np.exp(1j*pi_16), 0.95*np.exp(1j*(-pi_16)) ] 
+
+    num = np.poly(z)
+    denum = np.poly(p)
+
+    zplane(num, denum)
+
+    w, amp = signal.freqz(num, denum)
+    rep_freq(w, amp)
+
+    N = 500
+    n = np.arange(0, N)
+    x = [(np.sin(np.pi * n[i] / 16) + np.sin(np.pi * n[i] / 32)) for i in range(N)]
+
+    plt.stem(x)
     plt.show()
 
-    h = signal.lfilter(num, denum, impulsion)
-    plt.plot(h)
+    filtered = signal.lfilter(num, denum, x)
+
+    plt.stem(filtered)
     plt.show()
 
-    h_inv = signal.lfilter(denum, num, h)
-    plt.stem(h_inv)
-    plt.show()
+
+    def prob3():
+        
+
+
+
+
 
 if __name__ == '__main__':
-    prob1()
+    # prob1()
+    prob2()
