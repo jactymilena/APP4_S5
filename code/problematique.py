4574b90a9c6f34ad2c443163f51b47076c952288
Jacty <saej3101@usherbrooke.ca>
2023-02-27 11:29:12 -0500

    Matrix rotation

--- code/problematique.py
+++ code/problematique.py
@@ -38,8 +38,40 @@ def remove_aberrations(img, num, denum, plot=False):
     return img_filtered
 
 
-def rotate_img(img):
-    show_img(img)
+# def rotation_matrix(l, c, plot=False):
+#     mat = np.zeros((l, c))
+
+#     for i in range(c):
+#         for j in range(l):
+#             if i == l - j - 1:
+#                 mat[i, j] = 1
+
+#     if plot:
+#         print(mat)
+
+#     return mat
+
+
+def rotate_img(img, plot=False):
+    if plot:
+        show_img(img)
+
+    l = img.shape[0]
+    c = img.shape[1]
+    # T = rotation_matrix(l, c)
+    T = [[0, 1], [1, 0]]
+    mat = np.zeros((l, c))
+
+    for x in range(c):
+        for y in range(l):
+            y_img = l - 1 - y
+            res = np.dot(T, [x, y_img])
+            c_t = int(res[0])
+            l_t = int(res[1])
+            mat[l_t][c_t] = img[y_img][x]
+
+    show_img(mat)
+
 
 
 def main():
@@ -48,13 +80,15 @@ def main():
     filename2 = 'goldhill_aberrations.npy'
     filename3 = 'goldhill_rotate.png'
 
-    # img = read_img(filename1)
+    img = read_img(filename1)
     # img = read_img(filename2)
-    img = read_img(filename3, True)
+    # img = read_img(filename3, True)
 
     num, denum = inv_filter()
-    # img_filtered = remove_aberrations(img, num, denum, True)
-    rotate_img(img)
+    img_filtered = remove_aberrations(img, num, denum, True)
+    rotate_img(img_filtered)
+
+    
 
 
 if __name__ == '__main__':
